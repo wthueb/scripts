ddfb98884b0de6c9274b285b4ecdad1a61c256de
William Huebner <wilhueb@gmail.com>
2019-05-26 23:45:35 -0400

    fixed some stuff

--- create-url.py
+++ create-url.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import sys
 
-if len(sys.argv) != 3:
-    print('usage: ./create-url.py filename url')
 
-    exit(0)
+parser = argparse.ArgumentParser(description='creates shortcut to link')
 
-filename = sys.argv[1]
-url = sys.argv[2]
+parser.add_argument('filename', help='the name of the file for the shortcut with no extension')
 
-with open(filename + '.url', 'w') as f:
+parser.add_argument('url', help='the url to link to')
+
+args = parser.parse_args()
+
+with open(args.filename + '.url', 'w') as f:
     f.write('[InternetShortcut]\n')
-    f.write('URL=' + url + '\n')
+    f.write('URL=' + args.url + '\n')
     f.write('IconIndex=0')
 
-print(filename + '.url has been created, pointing to ' + url)
-print('path: ' + os.path.join(os.getcwd(), filename + '.url'))
+print(args.filename + '.url has been created, pointing to ' + args.url)
+print('path: ' + os.path.join(os.getcwd(), args.filename + '.url'))
