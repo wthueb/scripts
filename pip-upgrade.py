4e619df92abf9e90814f4816c45e1b89846fcec3
William Huebner <wilhueb@gmail.com>
2019-10-30 03:27:07 -0400

    python 2 is obsolete!

--- pip-upgrade.py
+++ pip-upgrade.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from subprocess import run
 
-res = run('pip3 freeze'.split(), capture_output=True)
+res = run('pip freeze'.split(), capture_output=True)
 
 packages = [s.strip() for s in res.stdout.decode('utf-8').split('\n') if s.strip()]
 packages = [p.split('==')[0] for p in packages]
 
 packages_str = ' '.join(packages)
 
-command = f'pip3 install --upgrade {packages_str}'
+command = f'pip install --upgrade {packages_str}'
 
 print(command)
 
--- to-pdf.py
+++ to-pdf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from os.path import basename, isfile
 import re
