cfbca1fbe65494a6d607422abc0c97b91ae3c423
William Huebner <wilhueb@gmail.com>
2020-07-08 08:50:24 -0400

    push command for pushover

---
+++ push.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+from pushover import Client
+
+class Parser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+
+        self.print_help()
+
+        sys.exit(1)
+
+
+parser = Parser(description=f'sends notification to pushover')
+
+parser.add_argument('--title', help='the title of the notification')
+parser.add_argument('--attachment', help='an image file to be attached')
+parser.add_argument('text', help='the file to upload. default: stdin', nargs='?', default=sys.stdin)
+
+args = parser.parse_args()
+
+# relies on ~/.pushoverrc to get api token and user key
+client = Client()
+
+client.send_message(args.text, title=args.title, attachment=args.attachment)
